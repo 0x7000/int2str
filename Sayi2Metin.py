e51ff36507c8c8848041f67366d87dc4efb8595a
command <rekreb81@gmail.com>
2018-11-09 02:29:25 +0300

    fix start with zero

--- Sayi2Metin.py
+++ Sayi2Metin.py
@@ -1,55 +1,62 @@
 class Sayi2Metin:
-    def __init__(self, sayi):
-        self.sayi = sayi
+	def __init__(self, sayi):
+		self.sayi = sayi
 
-    @property
-    def yaz(self):
-        if "," in self.sayi:
-            self.sayi = self.sayi.split(",")
-            kurus = self.sayi[1][0:2]
-            if kurus.startswith("0"):
-                return "{}, {} {}".format(self.cevir(self.sayi[0]), "Sıfır", self.cevir(kurus))
-            else:
-                return "{}, {}".format(self.cevir(self.sayi[0]), self.cevir(kurus))
-        else:
-            return "{}".format(self.cevir(self.sayi))
+	@property
+	def yaz(self):
+		if "," in self.sayi:
+			self.sayi = self.sayi.split(",")
+			kurus = self.sayi[1][0:2]
+			if kurus.startswith("0"):
+				return "{}, {} {}".format(self.cevir(self.sayi[0]), "Sıfır", self.cevir(kurus))
+			else:
+				return "{}, {}".format(self.cevir(self.sayi[0]), self.cevir(kurus))
+		else:
+			return "{}".format(self.cevir(self.sayi))
 
-    def cevir(self, sayi):
-        sayi = sayi.lstrip("0")
-        dizi = []
-        isaret = "+"
-        try:
-            for i in sayi:
-                dizi.append(i)
-            if dizi[0] == "-":
-                isaret = "Eksi"
-                dizi.pop(0)
-            dizi.reverse()
-            basamak = len(dizi)
-            if basamak == 1:sonuc = "{}".format(BIRLER[dizi[0]])
-            elif basamak == 2:sonuc = "{} {}".format(ONLAR[dizi[1]], BIRLER[dizi[0]])
-            elif basamak == 3:sonuc = "{} {} {}".format(YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
-            elif basamak == 4:sonuc = "{} {} {} {}".format(BINLER[dizi[3]], YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
-            elif basamak == 5:
-                if dizi[3] == "0":
-                    sonuc = "{} {} {} {}".format(ONLAR[dizi[4]]+" Bin", YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
-                else:
-                    sonuc = "{} {} {} {} {}".format(ONLAR[dizi[4]], BIRLER[dizi[3]]+" Bin", YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
-            elif basamak == 6:
-                if dizi[3] == "0":
-                    sonuc = "{} {} {} {} {}".format(YUZLER[dizi[5]], ONLAR[dizi[4]]+" Bin", YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
-                else:
-                    sonuc = "{} {} {} {} {} {}".format(YUZLER[dizi[5]], ONLAR[dizi[4]], BIRLER[dizi[3]]+" Bin", YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
-            else:
-                sonuc = "Aralık Dışında."
-            if isaret == "Eksi":
-                sonuc = "{} {}".format(isaret, sonuc.replace("  ", "").strip())
-            return sonuc.replace("  ","").strip()
-        except Exception as e:
-            return "Hata: {}".format(e)
+	def cevir(self, sayi):
+		if len(sayi) == 1 and sayi == "0":
+				return "Sıfır"
+		else:
+			sayi = sayi.lstrip("0")
+			dizi = []
+			isaret = "+"
+			try:
+				for i in sayi:
+					dizi.append(i)
+				if dizi[0] == "-":
+					isaret = "Eksi"
+					dizi.pop(0)
+				dizi.reverse()
+				basamak = len(dizi)
+				if basamak == 1:
+					sonuc = "{}".format(BIRLER[dizi[0]])
+				elif basamak == 2:
+					sonuc = "{} {}".format(ONLAR[dizi[1]], BIRLER[dizi[0]])
+				elif basamak == 3:
+					sonuc = "{} {} {}".format(YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
+				elif basamak == 4:
+					sonuc = "{} {} {} {}".format(BINLER[dizi[3]], YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
+				elif basamak == 5:
+					if dizi[3] == "0":
+						sonuc = "{} {} {} {}".format(ONLAR[dizi[4]]+" Bin", YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
+					else:
+						sonuc = "{} {} {} {} {}".format(ONLAR[dizi[4]], BIRLER[dizi[3]]+" Bin", YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
+				elif basamak == 6:
+					if dizi[3] == "0":
+						sonuc = "{} {} {} {} {}".format(YUZLER[dizi[5]], ONLAR[dizi[4]]+" Bin", YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
+					else:
+						sonuc = "{} {} {} {} {} {}".format(YUZLER[dizi[5]], ONLAR[dizi[4]], BIRLER[dizi[3]]+" Bin", YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
+				else:
+					sonuc = "Aralık Dışında."
+				if isaret == "Eksi":
+					sonuc = "{} {}".format(isaret, sonuc.replace("  ", "").strip())
+				return sonuc.replace("  ", "").strip()
+			except Exception as e:
+				return "Hata: {}".format(e)
 
 
-BIRLER = {"1":"Bir", "2":"İki", "3":"Üç", "4":"Dört", "5":"Beş", "6":"Altı", "7":"Yedi", "8":"Sekiz", "9":"Dokuz", "0":" "}
-ONLAR = {"1":"On", "2":"Yirmi", "3":"Otuz", "4":"Kırk", "5":"Elli", "6":"Altmış", "7":"Yetmiş", "8":"Seksen", "9":"Doksan", "0":" "}
-YUZLER = {"1":"Yüz", "2":"İki Yüz", "3":"Üç Yüz", "4":"Dört Yüz", "5":"Beş Yüz", "6":"Altı Yüz", "7":"Yedi Yüz", "8":"Sekiz Yüz", "9":"Dokuz Yüz", "0":" "}
-BINLER = {"1":"Bin", "2":"İki Bin", "3":"Üç Bin", "4":"Dört Bin", "5":"Beş Bin", "6":"Altı Bin", "7":"Yedi Bin", "8":"Sekiz Bin", "9":"Dokuz Bin", "0":" "}
+BIRLER = {"1": "Bir", "2": "İki", "3": "Üç", "4": "Dört", "5": "Beş", "6": "Altı", "7": "Yedi", "8": "Sekiz", "9": "Dokuz", "0": " "}
+ONLAR = {"1": "On", "2": "Yirmi", "3": "Otuz", "4": "Kırk", "5": "Elli", "6": "Altmış", "7": "Yetmiş", "8": "Seksen", "9": "Doksan", "0": " "}
+YUZLER = {"1": "Yüz", "2": "İki Yüz", "3": "Üç Yüz", "4": "Dört Yüz", "5": "Beş Yüz", "6": "Altı Yüz", "7": "Yedi Yüz", "8": "Sekiz Yüz", "9": "Dokuz Yüz", "0": " "}
+BINLER = {"1": "Bin", "2": "İki Bin", "3": "Üç Bin", "4": "Dört Bin", "5": "Beş Bin", "6": "Altı Bin", "7": "Yedi Bin", "8": "Sekiz Bin", "9": "Dokuz Bin", "0":" "}
--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from Sayi2Metin import Sayi2Metin
 
 
 def main():
-    sonuc = Sayi2Metin(input("Sayi: "))
-    print(sonuc.yaz) #@property ile çağrılırsa () kullanılmasına gerek yok
+	sonuc = Sayi2Metin(input("Sayi: "))
+	print(sonuc.yaz)
+	''''
+	#@property ile çağrılırsa () kullanılmasına gerek yok
+	'''
 
 
 if __name__ == "__main__":
-    main()
+	main()
 
 '''
-Property, özellik, nitelik anlamlarına gelmektedir. 
+Property, özellik, nitelik anlamlarına gelmektedir.
 Property dekoratörünün yaptığı en temel iş, bir metodu, nitelik gibi kullanılabilir hale getirmektir.
 '''
