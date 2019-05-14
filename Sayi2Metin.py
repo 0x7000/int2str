4ca8e5deb694d1e6fb931388324874ec153b0641
command <rekreb81@gmail.com>
2019-05-14 19:47:34 +0300

    add tkinter gui for madafakırs....

--- Sayi2Metin.py
+++ Sayi2Metin.py
@@ -1,6 +1,9 @@
-class Sayi2Metin:
+class Cevir:
 	def __init__(self, sayi):
 		self.sayi = sayi
+
+	def __del__(self):
+		print("Obje silindi....")
 
 	@property
 	def yaz(self):
@@ -15,15 +18,15 @@ class Sayi2Metin:
 			return "{}".format(self.cevir(self.sayi))
 
 	@staticmethod
-	def cevir(sayi):
-		if len(sayi) == 1 and sayi == "0":
+	def cevir(deger):
+		if len(deger) == 1 and deger == "0":
 			return "Sıfır"
 		else:
-			sayi = sayi.lstrip("0")
+			deger = deger.lstrip("0")
 			dizi = []
 			isaret = "+"
 			try:
-				for i in sayi:
+				for i in deger:
 					dizi.append(i)
 				if dizi[0] == "-":
 					isaret = "Eksi"
@@ -33,21 +36,50 @@ class Sayi2Metin:
 				if basamak == 1:
 					sonuc = "{}".format(BIRLER[dizi[0]])
 				elif basamak == 2:
-					sonuc = "{} {}".format(ONLAR[dizi[1]], BIRLER[dizi[0]])
+					sonuc = "{} {}".format(
+						ONLAR[dizi[1]],
+						BIRLER[dizi[0]])
 				elif basamak == 3:
-					sonuc = "{} {} {}".format(YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
+					sonuc = "{} {} {}".format(
+						YUZLER[dizi[2]],
+						ONLAR[dizi[1]],
+						BIRLER[dizi[0]])
 				elif basamak == 4:
-					sonuc = "{} {} {} {}".format(BINLER[dizi[3]], YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
+					sonuc = "{} {} {} {}".format(
+						BINLER[dizi[3]],
+						YUZLER[dizi[2]],
+						ONLAR[dizi[1]],
+						BIRLER[dizi[0]])
 				elif basamak == 5:
 					if dizi[3] == "0":
-						sonuc = "{} {} {} {}".format(ONLAR[dizi[4]]+" Bin", YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
+						sonuc = "{} {} {} {}".format(
+							ONLAR[dizi[4]]+" Bin",
+							YUZLER[dizi[2]],
+							ONLAR[dizi[1]],
+							BIRLER[dizi[0]])
 					else:
-						sonuc = "{} {} {} {} {}".format(ONLAR[dizi[4]], BIRLER[dizi[3]]+" Bin", YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
+						sonuc = "{} {} {} {} {}".format(
+							ONLAR[dizi[4]],
+							BIRLER[dizi[3]]+" Bin",
+							YUZLER[dizi[2]],
+							ONLAR[dizi[1]],
+							BIRLER[dizi[0]])
 				elif basamak == 6:
 					if dizi[3] == "0":
-						sonuc = "{} {} {} {} {}".format(YUZLER[dizi[5]], ONLAR[dizi[4]]+" Bin", YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
+						sonuc = "{} {} {} {} {}".format(
+							YUZLER[dizi[5]],
+							ONLAR[dizi[4]]+" Bin",
+							YUZLER[dizi[2]],
+							ONLAR[dizi[1]],
+							BIRLER[dizi[0]])
 					else:
-						sonuc = "{} {} {} {} {} {}".format(YUZLER[dizi[5]], ONLAR[dizi[4]], BIRLER[dizi[3]]+" Bin", YUZLER[dizi[2]], ONLAR[dizi[1]], BIRLER[dizi[0]])
+						sonuc = "{} {} {} {} {} {}".format(
+							YUZLER[dizi[5]],
+							ONLAR[dizi[4]],
+							BIRLER[dizi[3]]+" Bin",
+							YUZLER[dizi[2]],
+							ONLAR[dizi[1]],
+							BIRLER[dizi[0]])
 				else:
 					sonuc = "Aralık Dışında."
 				if isaret == "Eksi":
@@ -60,4 +92,4 @@ class Sayi2Metin:
 BIRLER = {"1": "Bir", "2": "İki", "3": "Üç", "4": "Dört", "5": "Beş", "6": "Altı", "7": "Yedi", "8": "Sekiz", "9": "Dokuz", "0": " "}
 ONLAR = {"1": "On", "2": "Yirmi", "3": "Otuz", "4": "Kırk", "5": "Elli", "6": "Altmış", "7": "Yetmiş", "8": "Seksen", "9": "Doksan", "0": " "}
 YUZLER = {"1": "Yüz", "2": "İki Yüz", "3": "Üç Yüz", "4": "Dört Yüz", "5": "Beş Yüz", "6": "Altı Yüz", "7": "Yedi Yüz", "8": "Sekiz Yüz", "9": "Dokuz Yüz", "0": " "}
-BINLER = {"1": "Bin", "2": "İki Bin", "3": "Üç Bin", "4": "Dört Bin", "5": "Beş Bin", "6": "Altı Bin", "7": "Yedi Bin", "8": "Sekiz Bin", "9": "Dokuz Bin", "0":" "}
+BINLER = {"1": "Bin", "2": "İki Bin", "3": "Üç Bin", "4": "Dört Bin", "5": "Beş Bin", "6": "Altı Bin", "7": "Yedi Bin", "8": "Sekiz Bin", "9": "Dokuz Bin", "0": " "}
--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python3
-from Sayi2Metin import Sayi2Metin
+from Sayi2Metin import Cevir
+from tkinter import *
 
 
 def main():
-	sonuc = Sayi2Metin(input("Sayi: "))
-	print(sonuc.yaz)
-	''''
-	#@property ile çağrılırsa () kullanılmasına gerek yok
-	'''
+	Pencere.geometry("450x250+10+10")
+	Pencere.mainloop()
+
+
+def hesapla():
+	sonuc = Cevir(xinput.get())
+	xlabel2.config(text=sonuc.yaz)
+
+
+Pencere = Tk()
+Pencere.title("Sayı2Metin")
+xlabel = Label(Pencere, text="Sayı Girişi : ")
+xlabel.place(x=10, y=10)
+xlabel2 = Label(Pencere, text="Sonuç : ")
+xlabel2.place(x=90, y=40)
+xinput = Entry(Pencere, bd=1)
+xinput.place(x=90, y=10)
+xbuton = Button(Pencere, text="Yazdır.", command=hesapla)
+xbuton.bind_all("<Return>", lambda x: hesapla())
+xbuton.place(x=10, y=30)
+xbuton2 = Button(Pencere, text="Sil.", command=lambda: xinput.delete(0, END))
+xbuton2.place(x=10, y=60)
 
 
 if __name__ == "__main__":
 	main()
-
-'''
-Property, özellik, nitelik anlamlarına gelmektedir.
-Property dekoratörünün yaptığı en temel iş, bir metodu, nitelik gibi kullanılabilir hale getirmektir.
-'''
